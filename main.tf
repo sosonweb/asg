ff450addfc3eddadfc7df1d2e5dbe3bae79d2c4e
ricky0517 <rithikrajvemula@sosonweb.com>
2024-04-23 16:08:58 -0500

    add iam role

--- main.tf
+++ main.tf
@@ -7,6 +7,8 @@ resource "aws_launch_configuration" "this" {
   image_id      = var.image_id
   instance_type = var.instance_type
   user_data     = var.user_data
+  iam_instance_profile = aws_iam_instance_profile.ssm_instance_profile.name
+
   lifecycle {
     create_before_destroy = true
   }
@@ -20,4 +22,51 @@ resource "aws_autoscaling_group" "this" {
   desired_capacity     = var.desired_capacity
   vpc_zone_identifier  = [var.subnet_id]
 
+}
+resource "aws_iam_role" "ssm_role" {
+  name = "SSMRoleForEC2"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "ssm_policy" {
+  name        = "SSMInstancePolicy"
+  description = "Policy to enable AWS Systems Manager service access"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = "ssm:*",
+        Resource = "*"
+      },
+      {
+        Effect = "Allow",
+        Action = "s3:*",
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_policy_attach" {
+  role       = aws_iam_role.ssm_role.name
+  policy_arn = aws_iam_policy.ssm_policy.arn
+}
+
+resource "aws_iam_instance_profile" "ssm_instance_profile" {
+  name = "SSMInstanceProfile"
+  role = aws_iam_role.ssm_role.name
 }
