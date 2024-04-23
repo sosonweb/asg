985a4219172a819f52350f0f54983b04c3ffba09
ricky0517 <143536544+ricky0517@users.noreply.github.com>
2024-04-23 14:28:52 -0500

    update userdata passing

--- main.tf
+++ main.tf
@@ -2,19 +2,11 @@ provider "aws" {
   region = var.region  # Update with your desired region
 }
 
-module "tf_aws_userdata" {
-  source = "git::ssh://git@github.com/sosonweb/tf_aws_userdata.git?ref=fire"
-  i_app_specific_user_data = module.tf_aws_userdata.op_userdata
-}
-output "op_userdata" {
-  value = module.tf_aws_userdata.op_userdata
-}
-
 resource "aws_launch_configuration" "this" {
   name_prefix   = "${var.name}-"
   image_id      = var.image_id
   instance_type = var.instance_type
-  user_data     = output.op_userdata
+  user_data     = var.user_data
   lifecycle {
     create_before_destroy = true
   }
