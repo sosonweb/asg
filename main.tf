82c646d71254cfbc56addd690aab37e2fd3d16f7
ricky0517 <rithikrajvemula@sosonweb.com>
2024-04-23 18:03:01 -0500

    add inbounb rule

--- main.tf
+++ main.tf
@@ -18,6 +18,13 @@ resource "aws_security_group" "asg_sg" {
   description = "Security group for ASG instances"
   vpc_id      = var.vpc_id  
 
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all inbound traffic on port 443 for HTTPS"
+  }
   egress {
     from_port   = 443
     to_port     = 443
